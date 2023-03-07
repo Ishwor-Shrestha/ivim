2e9badc57f4b17198ee30c1563559e969d14ead5
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-03-07 18:22:50 +0545

    Update lsp saga

--- lua/ishwor/plugins-setup.lua
+++ lua/ishwor/plugins-setup.lua
@@ -99,7 +99,7 @@ return packer.startup(function(use)
 
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
-	
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
